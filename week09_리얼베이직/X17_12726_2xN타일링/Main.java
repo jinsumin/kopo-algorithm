1b2c9fe7b095e56af5f443b1be6673c7b2cd46c1
jinsumin <suminjin@naver.com>
2022-06-06 20:29:53 +0900

    solve acmicpc 12726

---
+++ week09_리얼베이직/X17_12726_2xN타일링/Main.java
@@ -0,0 +1,33 @@
+package week09_리얼베이직.X17_12726_2xN타일링;
+
+// package tiling_2xn_11726;
+
+import java.util.Scanner;
+
+/**
+ * Created by SOOMIN on 2020-03-05.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Solution solution = new Solution();
+        System.out.println(solution.solution(n));
+    }
+}
+
+class Solution {
+    private static final int MOD = 10007;
+    public int solution(int n) {
+        int[] memo = new int[n + 1];
+        if(n == 1) {
+            return 1;
+        }
+        memo[0] = 1;
+        memo[1] = 1;
+        for(int i = 2; i <= n; i ++) {
+            memo[i] = (memo[i - 1] + memo[i - 2]) % MOD;
+        }
+        return memo[n];
+    }
+}
