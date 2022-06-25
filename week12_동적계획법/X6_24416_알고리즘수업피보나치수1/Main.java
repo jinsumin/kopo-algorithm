8aeae49a1c76d39edbb14c2ee45a445fb653907e
jinsumin <suminjin@naver.com>
2022-06-25 21:49:28 +0900

    solve acmicpc 24416

---
+++ week12_동적계획법/X6_24416_알고리즘수업피보나치수1/Main.java
@@ -0,0 +1,44 @@
+package week12_동적계획법.X6_24416_알고리즘수업피보나치수1;
+
+import java.util.Scanner;
+
+/**
+ * Created by soomin on 2022/06/25
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Solution solution = new Solution();
+        solution.solution(n);
+    }
+}
+
+class Solution {
+    private long countOfCaseA = 0;
+    private long countOfCaseB = 0;
+
+    public void solution(int n) {
+        fib(n);
+        fibonacci(n);
+        System.out.println(countOfCaseA + " " + countOfCaseB);
+    }
+
+    private void fib(int n) {
+        if (n == 1 || n == 2) {
+            countOfCaseA++;
+            return;
+        }
+        fib(n - 1);
+        fib(n - 2);
+    }
+
+    private void fibonacci(int n) {
+        int[] f = new int[n + 1];
+        f[1] = f[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            countOfCaseB++;
+            f[i] = f[i - 1] + f[i - 2];
+        }
+    }
+}
